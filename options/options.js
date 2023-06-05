cb5604ceb6f4f6f9e6bb30553b6c09687fde0f73
Crowdbotics <team@crowdbotics.com>
2023-06-05 18:34:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "beauty-opus-41262",
+  url: "https://beauty-opus-41262.botics.co",
+  api: "https://beauty-opus-41262.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
